820df1ababaa8f193e4e3d1de560b36093d54cfc
Pedro Veras <veraspedro1608@gmail.com>
2023-12-10 14:07:12 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Listar respostas da pergunta

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository";
 import { Answer } from "@/domain/forum/enterprise/entities/answer";
 
@@ -14,6 +15,14 @@ export class InMemoryAnswersRepository implements AnswersRepository {
         return answer
     }
 
+    async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+        const answers = this.items
+            .filter(item => item.questionId.toString() === questionId)
+            .slice((page - 1) * 20, page * 20)
+
+        return answers
+    }
+
     async create(answer: Answer): Promise<void> {
         this.items.push(answer)
     }
@@ -23,7 +32,7 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
         this.items[itemIndex] = answer
     }
-    
+
     async delete(answer: Answer) {
         const itemIndex = this.items.findIndex(item => item.id === answer.id)
 
