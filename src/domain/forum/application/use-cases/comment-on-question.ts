2d49f75f8fe8a25ba3fa241513fede2388e3df79
Pedro Veras <veraspedro1608@gmail.com>
2023-12-10 14:47:02 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Comentar na resposta

--- src/domain/forum/application/use-cases/comment-on-question.ts
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -1,7 +1,7 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { QuestionsRepository } from "../repositories/questions-repository";
 import { QuestionComment } from "../../enterprise/entities/question-comment";
-import { QuestionsCommentsRepository } from "../repositories/question-comments.repository";
+import { QuestionsCommentsRepository } from "../repositories/question-comments-repository";
 
 interface CommentOnQuestionUseCaseRequest {
   authorId: string
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,4 +1,4 @@
-import { QuestionsCommentsRepository } from "@/domain/forum/application/repositories/question-comments.repository";
+import { QuestionsCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
 import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
 
 export class InMemoryQuestionCommentsRepository implements QuestionsCommentsRepository {
